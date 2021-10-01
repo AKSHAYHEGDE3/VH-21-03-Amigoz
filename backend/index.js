05ff8b13e76614c395029abf92e72ffc2ae9485f
Akshay <akshayhegde158@gmail.com>
2021-10-01 15:10:42 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
+//const mongodb = "mongodb+srv://akshay:12345@cluster0.dbem1.mongodb.net/project?retryWrites=true&w=majority"
 const express=require("express");
-const cors = require('cors');
 const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
-const mongodb = "mongodb+srv://akshay:12345@cluster0.dbem1.mongodb.net/project?retryWrites=true&w=majority"
+
+//DATABASE VARIABLE
+const mongodb = "mongodb://localhost:27017/mydb"
+
+
 
 
 const app=express() //creating an express application
@@ -12,6 +18,9 @@ mongoose.connect(mongodb,{useNewUrlParser: true,  useUnifiedTopology: true  } ).
     console.log(`Listening at ${PORT}`)
 })}) //connecting to the database
 
+
+
+//MIDDLEWARE
 var corsOptions = {
     origin: 'http://localhost:3000',
     credentials: true,
@@ -19,8 +28,36 @@ var corsOptions = {
 }
 app.use(cors(corsOptions));
 
+
+
+
+
+//IMPORTING ROUTES
+const authRoutes= require("./routes/authRoutes.js")
+
+
+
+
+//TESTING COOKIES
+app.get("/set-cookies",(req,res)=>{
+    res.cookie("name","TEjas")
+    res.send("hi")
+})
+app.get('/get-cookies', (req, res) => {
+    const cookies = req.cookies;
+    console.log(req.cookies)
+    
+    res.json(cookies);
+})
+
+//USING ROUTES
+
+app.use(authRoutes);
+// app.use(postRoutes)
+// app.use(conversationRoutes)
+// app.use(messageRoutes)
+// app.use(userRoutes)
+
 app.get("",(req,res)=>{
     res.send("home")
 })
-
-
